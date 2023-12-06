2bb5b1f10f86b0c470a3927e75e3d24f62826f77
terao-takumi-personal <amane.astronomica@gmail.com>
2023-12-06 20:24:19 +0900

    フォント追加

--- app/game/page.js
+++ app/game/page.js
@@ -1,18 +1,20 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
 import MainTheme from "@/components/elements/MainTheme";
 import ModalWindow from "@/components/elements/ModalWindow";
 import Link from "next/link";
 import GameSequence from "@/features/components/GameSequence";
 
 export default function GamePage() {
-    return (
-        <MainTheme>
-            <ModalWindow>
-                <div className={styles.header}>
-                    <Link href='/'><p className={styles.linkText}>&gt; back</p></Link>
-                </div>
-                <GameSequence/>
-            </ModalWindow>
-        </MainTheme>
-    );
+  return (
+    <MainTheme>
+      <ModalWindow>
+        <div className={styles.header}>
+          <Link href="/">
+            <p className={styles.linkText}>&gt; back</p>
+          </Link>
+        </div>
+        <GameSequence />
+      </ModalWindow>
+    </MainTheme>
+  );
 }
--- components/elements/Code.js
+++ components/elements/Code.js
@@ -1,10 +1,23 @@
-import styles from '@/styles/components/elements/Code.module.css';
+import styles from "@/styles/components/elements/Code.module.css";
 
-export default function Code({code, index}) {
-    const typedWords = <span className={styles.typed} dangerouslySetInnerHTML={{__html: code.slice(0, index)}} />;
-    const unTypedWords = <span className={styles.untyped} dangerouslySetInnerHTML={{__html: code.slice(index)}} />;
+export default function Code({ code, index }) {
+  const typedWords = (
+    <span
+      className={styles.typed}
+      dangerouslySetInnerHTML={{ __html: code.slice(0, index) }}
+    />
+  );
+  const unTypedWords = (
+    <span
+      className={styles.untyped}
+      dangerouslySetInnerHTML={{ __html: code.slice(index) }}
+    />
+  );
 
-    return (
-        <p>{typedWords}{unTypedWords}</p>
-    );
+  return (
+    <p className={styles.code}>
+      {typedWords}
+      {unTypedWords}
+    </p>
+  );
 }
